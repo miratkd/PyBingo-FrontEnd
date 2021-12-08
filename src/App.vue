d4a8cbe4b824b98ce41639856a2f2c9865c1930c
lucas mira <lucas-mira1@hotmail.com>
2021-12-07 21:02:32 -0300

    init footer

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,34 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div class="app">
+    <router-view/>
   </div>
-  <router-view/>
+  <Footer/>
 </template>
 
+<script>
+import Footer from './components/Footer.vue'
+export default {
+  name: 'App',
+  components: { Footer }
+}
+</script>
+
 <style>
+.row{
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+}
+.app{
+  min-height: 90vh;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
 }
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+body{
+  margin: 0;
 }
 </style>
---
+++ src/components/Footer.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="footer__containear row">
+    <div class="footer__logo">
+      Py-Bingo
+    </div>
+    <div class="footer__info">
+      <div>test</div>
+      <div>test</div>
+      <div>test</div>
+      <div>test</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Footer'
+}
+</script>
+
+<style scoped>
+.footer__info{
+  border: 1px solid white;
+  border-radius: 5px;
+  width: 35%;
+  margin-right: 3%;
+  padding: 2px 5px;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+}
+.footer__logo{
+  font-size: xxx-large;
+  color: white;
+}
+.footer__containear{
+  justify-content: space-between;
+  height: 10vh;
+  background: gray;
+}
+</style>
